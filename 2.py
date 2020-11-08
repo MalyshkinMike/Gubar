08074865eee7a2dceefb3b2ef95ff11e72fb39aa
MrDrumm3r <mikhail.malishkin@gmail.com>
2020-11-08 21:58:06 +0400

    2 лаба готовая

--- 2.py
+++ 2.py
@@ -1,13 +1,14 @@
 from numpy import cos, sin, fabs
 from matplotlib import pyplot as canvas
 import numpy as np
-# 2 лаба работает, но не маскирована
-func1 = lambda x, y: cos(x + 0.5) - 2 - y
-func2 = lambda x, y: sin(y) - 2 * x - 1
-F = lambda x, y: np.array([[func1(x, y)], [func2(x, y)]])
+
+# 2 лаба
+eq1 = lambda x, y: cos(x + 0.5) - 2 - y  # eq - equation - уравнение
+eq2 = lambda x, y: sin(y) - 2 * x - 1
+equations = lambda x, y: np.array([[eq1(x, y)], [eq2(x, y)]])
 
 
-def graphics():
+def graph():
     f1 = lambda x: cos(x + 0.5) - 2
     f2 = lambda y: (sin(y) - 1) / 2
     x = np.linspace(-2, 0, 200)
@@ -26,17 +27,17 @@ J = np.array([
 ])
 
 
-def inverseJ(X, Y):
-    det = lambda x, y: -2 - sin(x + 0.5) * cos(y)
+def J_inv(X, Y):
+    determinant = lambda x, y: -2 - sin(x + 0.5) * cos(y)
     return np.array([
-        [(lambda x, y: cos(y) / det(x, y))(X, Y), (lambda x, y: 1 / det(x, y))(X, Y)],
-        [(lambda x, y: 2 / det(x, y))(X, Y), (lambda x, y: -sin(x + 0.5) / det(x, y))(X, Y)]
+        [(lambda x, y: cos(y) / determinant(x, y))(X, Y), (lambda x, y: 1 / determinant(x, y))(X, Y)],
+        [(lambda x, y: 2 / determinant(x, y))(X, Y), (lambda x, y: -sin(x + 0.5) / determinant(x, y))(X, Y)]
     ])
 
 
-def newton(x0, y0, eps):
+def Newton_method(x0, y0, eps):
     i = 0
-    [[x1], [y1]] = np.array([[x0], [y0]]) - inverseJ(x0, y0).dot(F(x0, y0))
+    [[x1], [y1]] = np.array([[x0], [y0]]) - J_inv(x0, y0).dot(equations(x0, y0))
     pre_x, pre_y = x0, y0
     while True:
         if (fabs(x1 - x0) <= eps and fabs(y1 - y0) <= eps):
@@ -47,13 +48,13 @@ def newton(x0, y0, eps):
         pre_y = y0
         x0 = x1
         y0 = y1
-        [[x1], [y1]] = np.array([[x0], [y0]]) - inverseJ(x0, y0).dot(F(x0, y0))
+        [[x1], [y1]] = np.array([[x0], [y0]]) - J_inv(x0, y0).dot(equations(x0, y0))
         i += 1
 
 
-def modified_newton(x0, y0, eps):
+def modified_Newton_method(x0, y0, eps):
     i = 0
-    [[x1], [y1]] = np.array([[x0], [y0]]) - inverseJ(x0, y0).dot(F(x0, y0))
+    [[x1], [y1]] = np.array([[x0], [y0]]) - J_inv(x0, y0).dot(equations(x0, y0))
     x00 = x0
     y00 = y0
     pre_x, pre_y = x0, y0
@@ -65,22 +66,21 @@ def modified_newton(x0, y0, eps):
         pre_x, pre_y = x00, y00
         x00 = x1
         y00 = y1
-        [[x1], [y1]] = np.array([[x00], [y00]]) - inverseJ(x0, y0).dot(F(x00, y00))
+        [[x1], [y1]] = np.array([[x00], [y00]]) - J_inv(x0, y0).dot(equations(x00, y00))
         i += 1
 
 
-graphics()
+graph()
 canvas.show()
 eps = 0.00001
 print('cos(x+0.5) - y = 2')
 print('sin(y) - 2x = 1')
-print('Начальное приближение -> ')
-x = float(input('\tВведите х -> '))
-y = float(input('\tВведите y -> '))
-newt_x, newt_y, newt_i = newton(x, y, eps)
-mod_newt_x, mod_newt_y, mod_newt_i = modified_newton(x, y, eps)
+print('Начальное приближение ► ')
+x = float(input('\tВведите х ► '))
+y = float(input('\tВведите y ► '))
+newt_x, newt_y, newt_i = Newton_method(x, y, eps)
+mod_newt_x, mod_newt_y, mod_newt_i = modified_Newton_method(x, y, eps)
 print('Метод Ньютона')
 print('\tx = ' + str(newt_x) + '\n\ty = ' + str(newt_y) + '\n\tколичество итераций (i) = ' + str(newt_i))
 print('Метод Ньютона (модифицированный)')
 print('\tx = ' + str(mod_newt_x) + '\n\ty = ' + str(mod_newt_y) + '\n\tколичество итераций (i) = ' + str(mod_newt_i))
-
