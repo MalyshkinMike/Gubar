e758cf0d6c47e5b9323357ec869cf6d3b41d3468
MrDrumm3r <mikhail.malishkin@gmail.com>
2020-11-08 21:57:40 +0400

    1 лаба. Написана так, что вродь претензий быть не должно. Про гибридный метод не смог ничего найти, вставил метод хорд из ее методички. (тоже из семейства методов дихотомии.) по этому обсудим еще сегодня

--- 1.py
+++ 1.py
@@ -1,15 +1,16 @@
 # первая лаба
 import matplotlib.pyplot as canvas
 import numpy as np
-import math
+from numpy import fabs
 
 
 def func(x):
     return (x ** 3 - 0.9 * x ** 2 - 22.0 * x - 30.8)
 
+
 def dichotomy(a, b, f, eps):
     i = 0
-    while (math.fabs(b - a) > 2 * eps):
+    while (fabs(b - a) > 2 * eps):
         i += 1
         x1 = (b + a) / 2
         y = f(x1)
@@ -23,9 +24,9 @@ def dichotomy(a, b, f, eps):
 
 def regula_falsi(a, b, f, eps):
     i = 0
-    while (math.fabs(b - a) > 2 * eps):
+    while (fabs(b - a) > 2 * eps):
         i += 1
-        x1 = a - (f(a)*(b-a))/(f(b) - f(a))
+        x1 = a - (f(a) * (b - a)) / (f(b) - f(a))
         y = f(x1)
         ymin = f(a)
         if ((y > 0 and ymin > 0) or (y < 0 and ymin < 0)):
@@ -33,6 +34,7 @@ def regula_falsi(a, b, f, eps):
         else:
             b = x1
     return x1, i
+
 
 gr_x_min = int(input("левая граница графика ►"))
 gr_x_max = int(input("правая граница графика ►"))
@@ -51,5 +53,7 @@ left_border = float(input("левая граница начального при
 right_border = float(input("правая граница начального приближения ► "))
 dichotomy_res = dichotomy(left_border, right_border, func, eps)
 regula_falsi_res = regula_falsi(left_border, right_border, func, eps)
-print("Метод дихотомии\n" + "\tРезультат ►" + str(dichotomy_res[0]) + "\n\tКоличество итераций ►" + str(dichotomy_res[1]) + "\n\tточность ►" + str(eps))
-print("Метод хорд\n" + "\tРезультат ►" + str(regula_falsi_res[0]) + "\n\tКоличество итераций ►" + str(regula_falsi_res[1]) + "\n\tточность ►" + str(eps))
+print("Метод дихотомии\n" + "\tРезультат ►" + str(dichotomy_res[0]) + "\n\tКоличество итераций ►" + str(
+    dichotomy_res[1]) + "\n\tточность ►" + str(eps))
+print("Метод хорд\n" + "\tРезультат ►" + str(regula_falsi_res[0]) + "\n\tКоличество итераций ►" + str(
+    regula_falsi_res[1]) + "\n\tточность ►" + str(eps))
